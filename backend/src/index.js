cefecfd1911398f536d15c1fdff9b4a32b9264ba
arpitha-5 <arpithamedarametla@gmail.com>
2026-02-21 19:04:22 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowed = [process.env.CLIENT_URL, "http://localhost:5173", "http://localhost:5174"]
+        .filter(Boolean);
+      if (!origin) return callback(null, true);
+      if (allowed.indexOf(origin) !== -1) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
